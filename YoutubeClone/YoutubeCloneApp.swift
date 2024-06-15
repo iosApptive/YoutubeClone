0f9924968166991a1beac317af0463deffc841e6
guyvensonc adet <createwithcodes@gmail.com>
2024-06-15 11:13:40 -0400

    Initial Commit

---
+++ YoutubeClone/YoutubeCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeCloneApp.swift
+//  YoutubeClone
+//
+//  Created by Mac on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
