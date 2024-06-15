5c69d353ecf9800902a43e91884041f36131910d
guyvensonc adet <createwithcodes@gmail.com>
2024-06-15 11:22:22 -0400

    created color theme that i will be using

---
+++ YoutubeClone/ShortsView.swift
@@ -0,0 +1,20 @@
+//
+//  ShortsView.swift
+//  YoutubeClone
+//
+//  Created by Mac on 6/15/24.
+//
+
+import SwiftUI
+
+struct ShortsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShortsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShortsView()
+    }
+}
---
+++ YoutubeClone/Util/Theme.swift
@@ -0,0 +1,14 @@
+//
+//  Theme.swift
+//  YoutubeClone
+//
+//  Created by Mac on 6/15/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct Theme {
+    static let ytBlack = Color("ytBlack")
+    static let ytGray = Color("ytGray")
+}
